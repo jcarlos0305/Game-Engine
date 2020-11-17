13d30c2c8faff62d7887e412137a456955af6eed
Juan Carlos Sánchez Guzmán <jcarlos0305@hotmail.com>
2020-11-17 12:49:33 +0100

    Setting up the base project

---
+++ Module.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Globals.h"
+
+class Application;
+
+class Module {
+public:
+
+	Module() {}
+
+	virtual bool Init() {
+		return true;
+	}
+
+	virtual update_status PreUpdate() {
+		return update_status::UPDATE_CONTINUE;
+	}
+
+	virtual update_status Update() {
+		return update_status::UPDATE_CONTINUE;
+	}
+
+	virtual update_status PostUpdate() {
+		return update_status::UPDATE_CONTINUE;
+	}
+
+	virtual bool CleanUp() {
+		return true;
+	}
+};
