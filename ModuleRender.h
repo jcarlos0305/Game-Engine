578647b6160e3e62c1e1cd4fd6ffb94412a2cae3
Juan Carlos Sánchez Guzmán <jcarlos0305@hotmail.com>
2020-11-22 13:29:09 +0100

    Added the model module, the mesh class & rendered the bakers's house

--- ModuleRender.h
+++ ModuleRender.h
@@ -1,10 +1,5 @@
 #pragma once
 #include "Module.h"
-#include "MathGeoLib/Math/float4x4.h"
-
-struct SDL_Texture;
-struct SDL_Renderer;
-struct SDL_Rect;
 
 class ModuleRender : public Module {
 public:
@@ -20,13 +15,5 @@ public:
 
 private:
 	void* context = nullptr;
-	unsigned int program = 0;
-	
-	/* Testing shaders & program module */
-	void CreateVBO();
-	void RenderVBO(float4x4 proj, float4x4 view, float4x4 model);
-	unsigned int vertex_shader = 0;
-	unsigned int fragment_shader = 0;
-	unsigned int vbo = 0;
-	unsigned int texture = 0;
+
 };
